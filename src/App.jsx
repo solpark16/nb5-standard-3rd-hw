d14191c74e7f28bfb0feb66b0fa2d02ce5ca272b
Sol Park <solpark16@gmail.com>
2024-05-20 19:33:03 +0900

    ex4 풀이 완료

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,20 @@ function App() {
   ];
 
   const [filteredStudents, setFilteredStudents] = useState(initialStudents);
-
   return (
     <div>
       <h1>학생 목록</h1>
       {/* TODO: FilterButtons 컴포넌트를 작성하고 필요한 props를 전달하세요. */}
       <FilterButtons
-      /* 필요한 props를 여기에 전달하세요. */
+        /* 필요한 props를 여기에 전달하세요. */
+        initialStudents={initialStudents}
+        setFilteredStudents={setFilteredStudents}
       />
 
       {/* TODO: StudentList 컴포넌트를 작성하고 필요한 props를 전달하세요. */}
       <StudentList
-      /* 필요한 props를 여기에 전달하세요. */
+        /* 필요한 props를 여기에 전달하세요. */
+        filteredStudents={filteredStudents}
       />
     </div>
   );
--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,9 +1,16 @@
 // TODO: StudentList 컴포넌트를 작성하세요. props로 학생 목록을 받아와서 표시해야 합니다.
-function StudentList(/* 필요한 props를 여기에 전달하세요 */ {}) {
+function StudentList(
+  /* 필요한 props를 여기에 전달하세요 */ { filteredStudents }
+) {
   return (
     <ul>
       {/* 여기에 학생 목록을 표시하는 로직을 작성하세요. */}
       {/* ex: 홍길동 (20세) - A등급 */}
+      {filteredStudents.map((student, index) => (
+        <li key={index}>
+          {student.name} - Age: {student.age}, Grade: {student.grade}
+        </li>
+      ))}
     </ul>
   );
 }
